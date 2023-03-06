08f89cff452e511b47e0152ac014997f0d2ea819
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2023-03-06 14:00:31 +0330

    variable name convention

--- Fundamentals-part1/script.js
+++ Fundamentals-part1/script.js
@@ -1,4 +1,35 @@
 let js = 'amazing'
-if(js==='amazing') alert('JavaScript is Fun!')
+// if(js==='amazing') alert('JavaScript is Fun!')
 
 console.log(40+22-24);
+
+// declaring variables
+let firstName = 'Naser'
+console.log(firstName);
+
+// variable name can not start with number
+// let 3years = 3;
+
+// variable names contain only: letters, numbers, under score(_) and dollar sign($)
+// let my&variable = 'test'
+let my_variable$ = 'that ok'
+
+// variable names should not be JavaScript reserved keyword like: new, function, name, etc.
+// let function ='something'
+// let new ='something'
+let name = 'my name'
+console.log(name);
+
+// variable names should NOT start with uppercase(it's not illegal but it's a convention)
+let Person = 'someone'
+// we choose all in uppercase letters for constant that we know will never change like: the number PI =3.1415 (convention)
+let PI = 3.1415
+
+// variable names should be descriptive
+// when you name a variable, it should be easy to understand what value the variable is holding.
+// more descriptive variables
+let myFirstJob = 'Civil Engineer'
+let myCurrentJpb = 'Programmer'
+// less descriptive variables
+let job1 = 'Civil Engineer'
+let job2 = 'Programmer'
