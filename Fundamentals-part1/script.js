cf964e06ba28f62ebb7fa0c87df32c0bb41189d8
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2023-03-19 13:11:40 +0330

    Operator Precedence and code challenge 1

--- Fundamentals-part1/script.js
+++ Fundamentals-part1/script.js
@@ -113,4 +113,56 @@ console.log(num) // 25
 // comparison operators produce Boolean values.
 console.log(ageNaser > ageSarah) // true
 
+// ** 10. Operator Precedence ** //
+/*
+JavaScript has a well-defined order of operator precedence, 
+So basically the order in which operators are executed.
+to check operator precedence table:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+
+And of course you don't need to know any of these precedence numbers.
+You just have to have a general idea of which operators are executed first.
+So usually all the math operators are executed before the comparison operators for example.
+*/
+
+let x,y;
+x = y = 25 - 10 - 5 
+console.log(x, y);
+
+// const averageAge = ageNaser + ageSarah / 2  // not ok, result 44.5 is not correct
+const averageAge = (ageNaser + ageSarah) / 2   // ok, result is 28
+console.log(ageNaser, ageSarah, averageAge);
+
+
+// ** Coding Challenge 1 ** //
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+Test data:
+Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/ 
+// const markMass = 78
+// const markHeight = 1.69
+// const johnMass = 92
+// const johnHeight = 1.95
+
+const markMass = 95
+const markHeight = 1.88
+const johnMass = 85
+const johnHeight = 1.76
+
+const markBMI = markMass / (markHeight**2)
+const johnBMI = johnMass / (johnHeight**2)
+const markHigherBMI = markBMI > johnBMI
+
+console.log(markBMI, johnBMI, markHigherBMI)
+
+
 
