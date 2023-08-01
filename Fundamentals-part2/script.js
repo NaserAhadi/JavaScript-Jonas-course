3d5e9412d278a72f878f64f16bd96ff3345adfae
NaserAhadi <naserahadi95@gmail.com>
2023-08-01 18:23:06 +0330

    kind of functions

--- Fundamentals-part2/script.js
+++ Fundamentals-part2/script.js
@@ -18,13 +18,63 @@ logger()
 logger()
 logger()
 
+// function fruitProccessor(apple, orange){
+//     const juice = `Juice with ${apple} apples and ${orange} oranges.`
+//     return juice
+// }
+
+// const appleJuice = fruitProccessor(5,0)
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProccessor(2, 4)
+// console.log(appleOrangeJuice);
+
+//** 4. Function Declarations vs. Expressions **/
+// Function declaration
+function calcAge1(birthYear){
+    return 2037 - birthYear
+}
+const age1 = calcAge1(1990)
+
+// Function expression
+const calcAge2 = function (birthYear){
+    return 2037 - birthYear
+}
+const age2 = calcAge2(1990)
+console.log(age1, age2)
+
+//** 5. Arrow Functions **/
+const calcAge3 = birthYear => 2037 - birthYear
+const age3 = calcAge3(1990)
+console.log(age3)
+
+const yearsUntilRetirement = birthYear => {
+    const age = 2037 - birthYear
+    const retirement = 65 - age
+    return retirement
+}
+console.log(yearsUntilRetirement(1990))
+
+const yearsUntilRetirementWithName = (birthYear, firstName) => {
+    const age = 2037 - birthYear
+    const retirement = 65 - age
+    return `${firstName} retires in ${retirement} years`
+}
+console.log(yearsUntilRetirementWithName(1990, 'Naser'))
+console.log(yearsUntilRetirementWithName(1991, 'Jonas'))
+
+
+// ** 6. Functions Calling Other Functions **//
+function cutFruitPieces(fruit){
+    return fruit * 4
+}
+
 function fruitProccessor(apple, orange){
-    const juice = `Juice with ${apple} apples and ${orange} oranges.`
+    const applePieces = cutFruitPieces(apple)
+    const orangePieces = cutFruitPieces(orange)
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`
     return juice
 }
 
-const appleJuice = fruitProccessor(5,0)
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProccessor(2, 4)
-console.log(appleOrangeJuice);
+console.log(fruitProccessor(2,3));
