0b7e1209a1e97d9479cc590524454bd4a20a3197
NaserAhadi <naserahadi95@gmail.com>
2023-08-30 17:10:56 +0330

    some examples

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+const xycbzd = 'dsjks';
+
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2037 - birthYear;
+console.log('x', x);
+console.log(xycbzd);
+
+// Todo Highlight extension:
+// FIXME:
+// INFO:
+// WARNING:
+// NOTE:
+// TODO:
+// TODO(string):
